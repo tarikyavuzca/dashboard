a8ad8c6cc445d2b305e7fec8d9737c8c4649cf3e
tarikyavuzca <tarikyavuzca@gmail.com>
2022-05-05 16:07:12 -0400

    completing the background of the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Welcome to Dashboard</div>;
+  return (
+    <div className="App">
+      <div className="MainPage"> </div>
+    </div>
+  );
 }
 
 export default App;
